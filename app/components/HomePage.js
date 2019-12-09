26bf69ef83b9f043650d193994076fbbf78d9dba
sergiga <srgarcia91@gmail.com>
2019-12-09 14:57:45 +0100

    Added calendar and exercise button

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -2,17 +2,30 @@ import * as React from 'react';
 import {
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
+import { CalendarList } from 'react-native-calendars';
 
 function HomePage() {
+  const navigateToExercises = () => {
+    console.log('navigate')
+  }
+
   return (
     <View style={styles.page}>
-      <View style={styles.calendar}>
-        <Text>CALENDAR</Text>
-      </View>
+      <CalendarList
+        horizontal={true}
+        pagingEnabled={true}
+      />
       <View style={styles.menu}>
-        <Text>MENU</Text>
+        <TouchableWithoutFeedback onPress={navigateToExercises}>
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Text style={styles.text}>Exercises</Text>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     </View>
   );
@@ -24,13 +37,28 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   calendar: {
-    flexGrow: 1,
+    flexShrink: 0,
     backgroundColor: 'white',
   },
   menu: {
-    flexShrink: 0,
-    height: 400,
-    backgroundColor: '#DFE2E9',
+    flexGrow: 1,
+    backgroundColor: '#e4e4e4',
+  },
+  buttonContainer: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    height: 64,
+    margin: 24
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#3F4149',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 24
   }
 });
 
