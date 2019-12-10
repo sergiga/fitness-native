50043b6222166e53c00cc202c54e56bc13541635
sergiga <srgarcia91@gmail.com>
2019-12-10 17:31:04 +0100

    Remove comunications folder

--- app/communications/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://localhost:8000';
-
-const WORKOUTS_URL = '/workouts/';
-
-export function getWorkouts() {
-  return axios.get(WORKOUTS_URL);
-};
